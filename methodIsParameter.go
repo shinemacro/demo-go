00f67d0663bc1f93f196f3131046c9bf628d9705
shinemacro <zhaoyaohong@aliyun.com>
2015-12-28 11:00:25 +0800

    add file: menthodIsParameter.go

---
+++ methodIsParameter.go
@@ -0,0 +1,20 @@
+package main 
+
+import (
+	"fmt"
+	"math"
+)
+
+func runMethodIsParameter() {
+   /* declare a function variable */
+   getSquareRoot := func(x float64) float64 {
+      return math.Sqrt(x)
+   }
+
+   /* use the function */
+   fmt.Println(getSquareRoot(9))
+}
+
+func init(){
+	runMethodIsParameter()
+}
